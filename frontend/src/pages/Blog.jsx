7c8fcfece00a8321139c50a39228a00fd96a0259
Karandeep Singh <singhkarandeep5440@gmail.com>
2025-06-08 06:25:45 +0530

    Blog page completed

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import { assets, blog_data, comments_data } from '../assets/assets'
 import Momemt from 'moment'
+import Footer from '../components/Footer'
+import Loader from '../components/Loader'
 
 export default function Blog() {
 
@@ -10,6 +12,8 @@ export default function Blog() {
 
   const [data, setData] = useState(null)
   const [comments, setComments] = useState([])
+  const [name, setName] = useState('')
+  const [content, setContent] = useState('')
 
   const fetchBlogData = async () => {
     const data = blog_data.find(item => item._id === id)
@@ -18,6 +22,10 @@ export default function Blog() {
 
   const fetchComments = async () => {
     setComments(comments_data)
+  }
+
+  const addComment = async (e) => {
+    e.preventDefault();
   }
 
   useEffect(() => {
@@ -61,10 +69,32 @@ export default function Blog() {
             ))}
           </div>
         </div>
+        {/* add comment section  */}
+        <div className='max-w-3xl mx-auto'>
+          <p className='font-semibold mb-4'>Add your comment </p>
+          <form onSubmit={addComment} className='flex flex-col items-start gap-4 max-w-lg'></form>
+          <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder='Name' required className='w-full p-2 border border-gray-300 
+            rounded outline-none' />
+
+          <textarea onChange={(e) => setContent(e.target.value)} value={content} placeholder='Comment' required className='w-full p-2 
+            border border-gray-300 rounded outline-none h-48'></textarea>
+          <button type="submit" className='bg-primary text-white rounded p-2 px-8
+             hover:scale-102 transition-all cursor-pointer'>Submit</button>
+        </div>
+        {/* share buttons */}
+        <div className='my-24 max-w-3xl mx-auto'>
+          <p className='font-semibold my-4'>Share this article on social media</p>
+          <div className='flex'>
+            <img src={assets.facebook_icon} width={50} alt="" />
+            <img src={assets.twitter_icon} width={50} alt="" />
+            <img src={assets.googleplus_icon} width={50} alt="" />
+          </div>
+        </div>
 
       </div>
+      <Footer/>
 
     </div>
 
-  ) : <div>Loading...</div>
+  ) : <Loader/>
 }
